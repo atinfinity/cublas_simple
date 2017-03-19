319daa113ec724534b74fea9667db957bb236f98
atinfinity <dandelion1124@gmail.com>
2017-03-19 23:12:39 +0900

    added a processing of matrix

--- cublas_simple.cpp
+++ cublas_simple.cpp
@@ -1,18 +1,38 @@
-﻿#include <cuda_runtime.h>
+﻿#include <cstdio>
+#include <cstdlib>
+#include <cuda_runtime.h>
 #include <cublas_v2.h>
 #include <iostream>
 #include "utility.hpp"
 
+#define M 6
+#define N 5
+#define IDX2C(i,j,ld) (((j)*(ld))+(i))
+
 int main(int argc, const char* argv[])
 {
+	float* a = 0;
+	a = (float*)malloc(M * N * sizeof (float));
+
+	for (int j = 0; j < N; j++)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			a[IDX2C(i,j,M)] = i;
+		}
+	}
+
+	float* devPtrA = 0;
+	cudaSafeCall(cudaMalloc((void**)&devPtrA, M*N*sizeof(*a)));
+
 	cublasHandle_t handle;
 	cublasSafeCall(cublasCreate(&handle));
-
-	int version = 0;
-	cublasSafeCall(cublasGetVersion(handle, &version));
-	std::cout << "cuBLAS version = " << version << std::endl;
-
+	cublasSafeCall(cublasSetMatrix(M, N, sizeof(*a), a, M, devPtrA, M));
+	cublasSafeCall(cublasGetMatrix(M, N, sizeof(*a), devPtrA, M, a, M));
 	cublasSafeCall(cublasDestroy(handle));
+
+	cudaSafeCall(cudaFree(devPtrA));	
+	free(a);
 
 	return 0;
 }
