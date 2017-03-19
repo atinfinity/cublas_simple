01a07d9379f17e485ee6d1cf39b52756a01aa71f
atinfinity <dandelion1124@gmail.com>
2017-03-19 23:00:08 +0900

    updated utility.hpp

--- utility.hpp
+++ utility.hpp
@@ -4,16 +4,26 @@
 #include <cublas_v2.h>
 #include <iostream>
 
+static inline void ___cudaSafeCall(cudaError_t status, const char* file, const int line, const char* func)
+{
+	if (cudaSuccess != status)
+	{
+		std::cerr << "[CUDA] failure, " << cudaGetErrorString(status) << " (status = " << status << "), " << "in function " << func << ". file " << file << ", line " << line << std::endl;
+	}
+}
+
 static inline void ___cublasSafeCall(cublasStatus_t status, const char* file, const int line, const char* func)
 {
 	if (CUBLAS_STATUS_SUCCESS != status)
 	{
-		std::cerr << "[cuBLAS] failure, status = " << status << ", " << "in function " << func << ". file " << file << ", line " << line << std::endl;
+		std::cerr << "[cuBLAS] failure, (status = " << status << "), " << "in function " << func << ". file " << file << ", line " << line << std::endl;
 	}
 }
 
-#if defined __GNUC__
+#if defined(__GNUC__)
+#define cudaSafeCall(expr)    ___cudaSafeCall(expr, __FILE__, __LINE__, __func__)
 #define cublasSafeCall(expr)  ___cublasSafeCall(expr, __FILE__, __LINE__, __func__)
 #else
+#define cudaSafeCall(expr)    ___cudaSafeCall(expr, __FILE__, __LINE__, __FUNCTION__)
 #define cublasSafeCall(expr)  ___cublasSafeCall(expr, __FILE__, __LINE__, __FUNCTION__)
 #endif
